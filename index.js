56713cf8512627718120ad8ad0248f14c92557eb
mothisbala <82767344+mothisbala@users.noreply.github.com>
2021-10-07 23:02:58 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+var http = require('http');
+var fs = require('fs');
+const PORT= process.env.PORT||5000;
+ var server= http.createServer(function(req,res){
+    res.writeHead(200,{'Content-type':'text/html'});
+    fs.readFile('./index.html',null,function(error,data){
+        if(error){
+            res.writeHead(404);
+            res.write('File not Found!')
+        }else{
+            res.write(data);
+
+        }
+        res.end();
+
+
+    });
+    
+
+
+});
+server.listen(PORT,()=>{
+        console.log('Server running on port'+PORT)
+});
